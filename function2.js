b0edbecc936438678fc6a4620bd613190ef9c47d
souzartedev <cyber.souza@hotmail.com>
2024-10-27 20:39:47 -0300

    Segundo commit do exercício

--- function2.js
+++ function2.js
@@ -5,3 +5,17 @@ function pedido (item, valor, nome = "Cliente"){
     console.log("Valor do pedido é de R$ " + valor);
     console.log("Esse pedido é de: " + nome)
 }
+
+/*
+Segunda versão da função
+*/
+
+compra ("Hambuguer de frango", 11.00);
+
+
+function compra(lanche, preco, solicitante = "Consumidor Final"){
+    console.log("-----------");
+    console.log("Pedido realizado: " + lanche);
+    console.log("O custo de R$ " + preco);
+    console.log("Quem solicitou foi: " + solicitante);
+}
